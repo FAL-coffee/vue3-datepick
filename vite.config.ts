66925062f0dcb32ef30e7b96d7904d1e49a4cc6c
FAL-coffee <fal.engineer.2001@gmail.com>
2024-06-24 01:03:02 +0900

    :adhesive_bandage: Typo

--- vite.config.ts
+++ vite.config.ts
@@ -5,45 +5,48 @@ import { defineConfig } from "vite"
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js"
 import dts from "vite-plugin-dts"
 
-const isDevMode = process.env.DEV_MODE === "local"
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    dts({
-      entryRoot: "src/DatePicker",
-      outDir: "dist",
-    }),
-    cssInjectedByJsPlugin(),
-  ],
-  build: {
-    lib: {
-      // @ts-ignore
-      entry: resolve(__dirname, "src/DatePicker"),
-      name: "DatePicker",
-      fileName: "vue3-datepick",
-      formats: ["es", "umd"],
-    },
-    rollupOptions: {
-      external: ["vue", "date-fns"],
-      output: {
-        globals: {
-          vue: "Vue",
+export default defineConfig(
+  (() => {
+    const isDevMode = process.env.DEV_MODE === "true"
+    return {
+      plugins: [
+        vue(),
+        dts({
+          entryRoot: "src/DatePicker",
+          outDir: "dist",
+        }),
+        cssInjectedByJsPlugin(),
+      ],
+      build: {
+        lib: {
+          // @ts-ignore
+          entry: resolve(__dirname, "src/DatePicker"),
+          name: "DatePicker",
+          fileName: "vue3-datepick",
+          formats: ["es", "umd"],
+        },
+        rollupOptions: {
+          external: ["vue", "date-fns"],
+          output: {
+            globals: {
+              vue: "Vue",
+            },
+          },
         },
       },
-    },
-  },
-  css: {
-    modules: {
-      scopeBehaviour: "local",
-    },
-    preprocessorOptions: {
-      scss: {
-        additionalData: isDevMode
-          ? `@import "dev/_variables.scss";`
-          : `@import "src/DatePicker/styles/variables.scss";`,
+      css: {
+        modules: {
+          scopeBehaviour: "local",
+        },
+        preprocessorOptions: {
+          scss: {
+            additionalData: isDevMode
+              ? `@import "dev/_variables.scss";`
+              : `@import "src/DatePicker/styles/variables.scss";`,
+          },
+        },
       },
-    },
-  },
-})
+    }
+  })()
+)
