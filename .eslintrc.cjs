d612c336ae501d736e014442fd18789a2c06a7f6
ふぁる <73627117+FAL-coffee@users.noreply.github.com>
2024-07-22 00:37:27 +0900

    Chore/configure vitest and ci (#12)
    * :heavy_plus_sign: Add Vitest
    
    * :hammer: Configure Vitest, Add Example test code, And add test flow for CI.
    
    * :hammer: Change parser.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,8 +10,9 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
-  parser: '@typescript-eslint/parser',
+  parser: 'vue-eslint-parser',
   parserOptions: {
+    parser: '@typescript-eslint/parser',
     ecmaVersion: 2020,
     sourceType: 'module',
   },
